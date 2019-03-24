b3b9728c34c3286cd9b0d5a85218207f87e4cc06
Grace Park <parkg748@newschool.edu>
2019-03-24 11:19:17 -0700

    connect vuex to vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="active item">
+      Image storage
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppHeader'
+  }
+</script>
